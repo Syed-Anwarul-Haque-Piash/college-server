8e795f877113a1020cbb01134ff3626fe4cd33c1
Syed-Anwarul-Haque-Piash <syed88783@gmail.com>
2023-07-23 18:26:13 +0600

    allcollege

--- index.js
+++ index.js
@@ -33,6 +33,11 @@ async function run() {
         res.send(result);
         console.log(college);
     })
+    app.get('/allcollege',async(req,res)=>{
+      
+        const result=await collegeCollection.find().toArray();
+        res.send(result);
+    })
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
